09100b1754f40905748569af583991748b4a8589
natnanchai6704062610348 <s6704062610348@email.kmutnb.ac.th>
2026-07-02 15:43:02 +0700

    Add files via upload

---
+++ console.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+public class console {
+    public static void main(String[] args) {
+        String string = JOptionPane.showInputDialog("year");
+        System.out.println(string);
+    }
+}
